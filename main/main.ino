5c091a22d357c3e4eeb788e3b063c68af21a756d
Starbuck7410 <starbuck7410@gmail.com>
2025-04-29 16:24:16 +0300

    alarm going off when pressing the button

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,50 @@
 
 // the setup function runs once when you press reset or power the board
+int output_pin = 10;
+int button_pushed = 0; // 1 if button is pushed and 0 if it is'nt 
+int interruptPin = 2;
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(output_pin, OUTPUT);
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), stopAlarm, FALLING);
+
+  // pinMode(LED_BUILTIN, OUTPUT); // meant for debugging 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(50);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(50);                      // wait for a second
+
+  if (!button_pushed)
+  {
+    outputNote(1200, 500);
+    outputNote(800, 500);
+  }
+  
+}
+
+void outputNote(int frequency, long time){
+
+  int cycle_us = 1000000 / frequency;
+  for (int i = 0; i <= (time * frequency) / 1000 ; i++){
+
+    if(!button_pushed)
+    {
+      digitalWrite(output_pin, HIGH);  // turn the LED on (HIGH is the voltage level)
+      delayMicroseconds((cycle_us / 2));
+      
+      
+      digitalWrite(output_pin, LOW);   // turn the LED off by making the voltage LOW
+      delayMicroseconds((cycle_us / 2));
+    }
+    
+  }
+}
+
+void stopAlarm()
+{
+  button_pushed = 1;
+  return;
 }
